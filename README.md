a40176416c6640b216e5ee6a24082906acd37509
Alberto Ortiz Flores <aortiz@nyu.edu>
2018-05-04 10:56:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Dragon
+========
+
+To run agartha for development site, use at command line: 
+
+agartha forge
+
+To pass in the enviromental variable for production, use
+
+AGARTHA_ENVIRONMENT=production agartha forge
---
+++ app/pages/front/app.js
@@ -0,0 +1,157 @@
+"use strict"
+
+/**
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2123, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3233}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 67, "height": 102}, {"width": 133, "height": 203}, {"width": 266, "height": 405}, {"width": 531, "height": 809}, {"width": 1062, "height": 1617}, {"width": 2123, "height": 3233}], "height": 3233, "width": 2123, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000001_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2095, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3229}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 101}, {"width": 131, "height": 202}, {"width": 262, "height": 404}, {"width": 524, "height": 808}, {"width": 1048, "height": 1615}, {"width": 2095, "height": 3229}], "height": 3229, "width": 2095, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000002_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2120, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3233}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 67, "height": 102}, {"width": 133, "height": 203}, {"width": 265, "height": 405}, {"width": 530, "height": 809}, {"width": 1060, "height": 1617}, {"width": 2120, "height": 3233}], "height": 3233, "width": 2120, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000003_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2087, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3229}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 101}, {"width": 131, "height": 202}, {"width": 261, "height": 404}, {"width": 522, "height": 808}, {"width": 1044, "height": 1615}, {"width": 2087, "height": 3229}], "height": 3229, "width": 2087, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000004_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2112, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3237}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 102}, {"width": 132, "height": 203}, {"width": 264, "height": 405}, {"width": 528, "height": 810}, {"width": 1056, "height": 1619}, {"width": 2112, "height": 3237}], "height": 3237, "width": 2112, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000005_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2099, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3221}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 101}, {"width": 132, "height": 202}, {"width": 263, "height": 403}, {"width": 525, "height": 806}, {"width": 1050, "height": 1611}, {"width": 2099, "height": 3221}], "height": 3221, "width": 2099, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000006_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2100, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3237}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 102}, {"width": 132, "height": 203}, {"width": 263, "height": 405}, {"width": 525, "height": 810}, {"width": 1050, "height": 1619}, {"width": 2100, "height": 3237}], "height": 3237, "width": 2100, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000007_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2091, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3229}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 101}, {"width": 131, "height": 202}, {"width": 262, "height": 404}, {"width": 523, "height": 808}, {"width": 1046, "height": 1615}, {"width": 2091, "height": 3229}], "height": 3229, "width": 2091, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000008_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2106, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3233}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 66, "height": 102}, {"width": 132, "height": 203}, {"width": 264, "height": 405}, {"width": 527, "height": 809}, {"width": 1053, "height": 1617}, {"width": 2106, "height": 3233}], "height": 3233, "width": 2106, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000009_d.jp2"}
+  {"profile": ["http://iiif.io/api/image/2/level2.json", {"supports": ["canonicalLinkHeader", "profileLinkHeader", "mirroring", "rotationArbitrary", "regionSquare", "sizeAboveFull"], "qualities": ["default", "bitonal", "gray", "color"], "formats": ["jpg", "png", "gif", "webp"]}], "tiles": [{"width": 2075, "scaleFactors": [1, 2, 4, 8, 16, 32], "height": 3225}], "protocol": "http://iiif.io/api/image", "sizes": [{"width": 65, "height": 101}, {"width": 130, "height": 202}, {"width": 260, "height": 404}, {"width": 519, "height": 807}, {"width": 1038, "height": 1613}, {"width": 2075, "height": 3225}], "height": 3225, "width": 2075, "@context": "http://iiif.io/api/image/2/context.json", "@id": "http://localhost:9080/loris/princeton_aco000319_n000010_d.jp2"}
+*/
+
+function tiles() {
+  const dragon = document.getElementById('dragon');
+  const loris = dragon.getAttribute('data-loris');
+  const identifier = dragon.getAttribute('data-identifier');
+  const sequenceCount = parseInt(dragon.getAttribute('data-sequence-count'), 10);
+  const sequence = parseInt(dragon.getAttribute('data-sequence'), 10);
+  const template = dragon.getAttribute('data-template');
+  var templateFn = doT.template(template);
+  let tiles = [];
+  let length = sequenceCount + 1;
+  try {
+    while (--length) { // no index 0
+      tiles.push(templateFn({
+        sequence: (length).toString().padStart(6, '0'), // pattern to match [0-9]{6} 
+    	loris: loris,
+    	identifier: identifier
+      }))
+    }
+  }
+  catch (e) {
+    console.log(e)
+  }
+  console.log(tiles)
+  return tiles;
+}
+
+function fullscreenOn() {
+  const docElm = document.documentElement;
+  // Go fullscreen
+  if (docElm.requestFullscreen) {
+    docElm.requestFullscreen();
+  }
+  else if (docElm.msRequestFullscreen) {
+    docElm.msRequestFullscreen();
+  }
+  else if (docElm.mozRequestFullScreen) {
+    docElm.mozRequestFullScreen();
+  }
+  else if (docElm.webkitRequestFullScreen) {
+    docElm.webkitRequestFullScreen();
+  }
+  // Y.CrossFrame.postMessage("parent", JSON.stringify({fire: 'button:button-fullscreen:on'}));
+}
+
+function fullscreenOff() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+  else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+  else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  }
+  else if (document.webkitCancelFullScreen) {
+    document.webkitCancelFullScreen();
+  }
+  //if (fullscreenButton) {
+    //fullscreenButton.blur();
+  //}
+  // Y.CrossFrame.postMessage("parent", JSON.stringify({fire: 'button:button-fullscreen:off'}));
+}
+
+$(document).ready(function() {
+
+  const $dragon = $('#dragon')
+
+  const configuration = $dragon.data()
+
+  const ee = new EventEmitter()
+  
+  const events = [
+    'viewer-metadata', 
+    'viewer-next', 
+    'viewer-fullscreen'
+  ]
+
+  ee.defineEvents(events)
+
+  ee.addListener('viewer-metadata', () => {
+	$('body').toggleClass('pagemeta-hidden')
+  })
+  
+  ee.addListener('viewer-fullscreen', () => {
+	fullscreenOn()
+  })
+  
+  ee.addListener('viewer-next', () => {
+	let next = viewer._sequenceIndex + 1
+	if (viewer.navPrevNextWrap && next >= viewer.tileSources.length) {
+	  next = 0
+	}
+    viewer.goToPage(next)
+  })
+  
+  ee.addListener('viewer-previous', () => {
+    var previous = viewer._sequenceIndex - 1
+    if (viewer.navPrevNextWrap && previous < 1) {
+      previous = 1
+	}
+    viewer.goToPage(previous)
+  })
+
+  const viewer = OpenSeadragon({
+    id: $dragon.attr('id'),
+    // Prepends the prefixUrl to navImages paths, which is very 
+    // useful since the default paths are rarely useful for production
+    // environments.
+    prefixUrl: "/viewer/js/openseadragon-bin-2.3.1/images/",
+    preload: false,
+    // Render the best closest level first, ignoring the lowering levels
+    // which provide the effect of very blurry to sharp. It is 
+    // recommended to change setting to true for mobile devices.
+    immediateRender: false,
+    preserveViewport: configuration.preserveViewport,
+    showRotationControl: configuration.rotation,
+    degrees: configuration.degrees,
+    initialPage: configuration.sequence - 1,
+    // Tile source(s) to open initially. This is a complex parameter; see {@link OpenSeadragon.Viewer#open} for details.
+    tileSources: tiles(),
+    // Set to false to prevent the appearance of the default navigation
+    // controls. Note that if set to false, the customs buttons set by 
+    // the options zoomInButton, zoomOutButton etc, are rendered inactive.
+    showNavigationControl: false,
+    // Enable touch rotation on tactile devices
+    gestureSettingsTouch: {
+      pinchRotate: true
+    }
+  })
+
+  viewer.addHandler("page", function (data) {
+    console.log(data)
+  })
+
+  $('.button').on('click', function(event) {	
+	event.preventDefault()
+	const $this = $(this)
+	$this.toggleClass('on')
+    return ee.emitEvent($this.attr('data-emmit'), this)    
+  })
+
+})
