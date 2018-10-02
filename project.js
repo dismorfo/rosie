de9050efd6c0c0e7ed60691e482d080eb5cd8da7
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-10-02 15:01:49 -0400

    update

--- project.js
+++ project.js
@@ -1,10 +1,17 @@
+'use strict'
+
+const agartha = process.agartha
+const appUrl = agartha.get('ROSIE_APP_URL')
+const appRoot = agartha.get('ROSIE_APP_ROOT')
+const provider = agartha.get('ROSIE_PROVIDER')
+
 module.exports = exports = {
   appName: 'The Real Rosie the Riveter Project',
   shortName: 'rosie',
-  appUrl: 'http://127.0.0.1:8080',
-  appRoot: 'http://127.0.0.1:8080',
+  appUrl: appUrl,
+  appRoot: appRoot,
   relic: 'scaffold',
   version: '0.0.1',
   hephaestus: '2.0.0',
-  provider: 'http://media.local'
+  provider: provider
 }
